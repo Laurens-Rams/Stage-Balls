3b4757656214664b408a197a42bb0c5181f85e29
Emily Kolar <emkolardev@gmail.com>
2018-07-02 23:15:17 -0500

    remove unused code

--- ColorBall/Settings.swift
+++ ColorBall/Settings.swift
@@ -15,15 +15,4 @@ struct Settings {
     static let PRESENT_INTERVAL: Double = 21600
     static let CURRENT_STAGE_KEY = "CURRENT_STAGE"
     static let HIGH_SCORE_KEY = "HIGH_SCORE"
-    
-//    static let ballColors: [UIColor] = [
-//        UIColor(red: 48/255, green: 153/255, blue: 232/255, alpha: 1.0),
-//        UIColor(red: 247/255, green: 117/255, blue: 132/255, alpha: 1.0),
-//        UIColor(red: 63/255, green: 139/255, blue: 138/255, alpha: 1.0),
-//        UIColor(red: 223/255, green: 175/255, blue: 71/255, alpha: 1.0),
-//        UIColor(red: 124/255, green: 45/255, blue: 243/255, alpha: 1.0),
-//        UIColor(red: 117/255, green: 228/255, blue: 179/255, alpha: 1.0),
-//        UIColor(red: 255/255, green: 00/255, blue: 00/255, alpha: 1.0),
-//        UIColor(red: 56/255, green: 56/255, blue: 56/255, alpha: 1.0),
-//    ]
 }
